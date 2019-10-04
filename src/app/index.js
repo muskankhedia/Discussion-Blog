163e306cd7b3fc08304ed08f5f1579f2ac7780fa
muskankhedia <muskan.khedia2000@gmail.com>
2019-10-04 15:55:52 +0530

    linked add comments with backend

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,57 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const express = require('express'),
-      bodyParser = require('body-parser'),
-      app = express(),
-      port = process.env.PORT || 5000,
-      url = '0.0.0.0';
+	bodyParser = require('body-parser'),
+	app = express(),
+	JsonDB = require('node-json-db').JsonDB,
+	Config = require('node-json-db/dist/lib/JsonDBConfig').Config,
+	port = process.env.PORT || 5000,
+	url = '0.0.0.0';
 
+var db = new JsonDB(new Config('./datastore/comments-store', true, true, '/'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended:true,
+	extended:true,
 }));
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+app.use((_,res,next)=>{
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
-console.log("Connected")
-app.get('/', (req ,res) => {
-    res.send('this is working');
+console.log('Connected');
+
+app.get('/', (_ ,res) => {
+	res.send('this is working');
+});
+
+app.post('/addComment', (req, res) => {
+
+	let name = req.body.name,
+		comment = req.body.comment;
     
-})
- 
-app.listen(3000)
+	var obj ={
+		name: name,
+		comment: comment
+	};
+
+	db.push('/comments/1', obj);
+	res.send('Yes');
+});
+
+app.post('/upvoteComment', (_req, _res) => {
+
+});
+
+app.post('/downvoteComment', (_req, _res) => {
+
+});
+
+const server = app.listen(port, url, e => {
+	if(e) throw e;
+	else {
+		console.warn('Running at \n'+server.address().address + '\t' +server.address().port);
+        
+	}
+});
--- src/front-end/main.js
+++ src/front-end/main.js
@@ -1,19 +1,44 @@
+/* eslint-disable no-undef */
 var app = angular.module('commenting-app', ['ngRoute']);
 
 var global = {
-    url:'http://0.0.0.0:5000',
-    refresh:true,
-}
+	url:'http://0.0.0.0:5000',
+	refresh:true,
+};
 
-app.config(function($routeProvider,$locationProvider) {
-    $routeProvider
-    .when('/',{
-        templateUrl:'./html_components/comments.html',
-        controller:'commentController',
-        title:'Commenting App',
-    })
-})
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/',{
+			templateUrl:'./html_components/comments.html',
+			controller:'commentController',
+			title:'Commenting App',
+		});
+});
 
-app.controller('commentController', function($scope,$location,$rootScope,$http) {
-    console.warn('initiated project')
-})
+app.controller('commentController', function($scope, $http) {
+	console.warn('initiated project');
+	console.log($scope.newComment);
+	$scope.name = '';
+	$scope.newComment = '';
+	$scope.addComment = function() {
+		console.log('clicked');
+		console.log($scope.name);
+		console.log($scope.newComment);
+		let data = 'name='+$scope.name+'&comment='+$scope.newComment;
+		$http(
+			{
+				url: global.url+'/addComment',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded' 
+				},
+				data: data
+			}
+		)
+			.then( resp => {
+				res = resp.data;
+				console.log(res);
+			});
+	};
+
+});
